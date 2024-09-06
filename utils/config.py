5b911f7ca766ea468d95aaab5a680474ef633640
David Poslušný <david.poslusny@email.cz>
2024-09-06 18:12:32 +0200

    feat: add config class

---
+++ utils/config.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class Config:
+    """ Configuration class for training hyperparameters. """
+    def __init__(self):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.batch_size = 8
+        self.lr = 5e-5
+        self.num_epochs = 5
+        self.optimizer = torch.optim.AdamW
+        self.criterion = nn.CrossEntropyLoss()
