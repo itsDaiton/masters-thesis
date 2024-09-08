914af4dad8a3516eb5adc45e275cb3c3ceaa7329
David Poslušný <david.poslusny@email.cz>
2024-09-08 14:59:22 +0200

    fix: 2 epochs for testing only

--- utils/config.py
+++ utils/config.py
@@ -7,6 +7,6 @@ class Config:
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.batch_size = 8
         self.lr = 5e-5
-        self.num_epochs = 5
+        self.num_epochs = 2
         self.optimizer = torch.optim.AdamW
         self.criterion = nn.CrossEntropyLoss()
