793526be3b7f9be170f820d81f4ee448e3e00b95
David Poslušný <david.poslusny@email.cz>
2024-09-08 13:12:47 +0200

    feat: add regnet teacher for deit

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from transformers import ViTForImageClassification, DeiTForImageClassificationWithTeacher
 import torch.nn as nn
+from torchvision.models.regnet import regnet_y_16gf, RegNet_Y_16GF_Weights
 
 class ViT(nn.Module):
     """ Vision Transformer (ViT) model pre-trained on ImageNet-1k. """
@@ -32,3 +33,13 @@ class DeiT(nn.Module):
         class_logits = outputs.logits
         distillation_logits = outputs.distillation_logits
         return class_logits, distillation_logits
+    
+class RegNet(nn.Module):
+    """ RegNetY-16GF model pre-trained on ImageNet-1k. This model serves as a teacher model for DeiT. """
+    
+    def __init__(self, num_classes):
+        super(RegNet, self).__init__()
+        self.regnet = regnet_y_16gf(weights=RegNet_Y_16GF_Weights.DEFAULT)
+        
+    def forward(self, x):
+        return self.regnet(x)
