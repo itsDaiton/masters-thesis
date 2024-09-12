6584fd7d834ff400ff22f59b9cd6efde5f6cc8f8
David Poslušný <david.poslusny@email.cz>
2024-09-12 16:01:27 +0200

    feat: new training function

--- src/train.py
+++ src/train.py
@@ -1,17 +1,20 @@
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from utils.models_utils import get_last_layer
+from utils.train_utils import print_training_results
 
-def linear_probe(model, train_loader: DataLoader, val_loader: DataLoader, config):
+    
+def train(model, train_loader, val_loader, mode, config):
     model.to(config.device)
     
-    for param in model.parameters():
-        param.requires_grad = False
-        
-    for param in model.backbone.classifier.parameters():
-        param.requires_grad = True
-        
-    optimizer = config.optimizer(model.parameters(), lr=config.lr)
+    if mode == 'linear_probing':
+        for param in model.parameters():
+            param.requires_grad = False
+        for param in get_last_layer(model, model.model_name).parameters():
+            param.requires_grad = True
+    
+    optimizer = config.optimizer(filter(lambda p: p.requires_grad, model.parameters()), lr=0.01)
     criterion = config.criterion
     
     for epoch in range(config.num_epochs):
@@ -20,30 +23,30 @@ def linear_probe(model, train_loader: DataLoader, val_loader: DataLoader, config
         train_correct = 0
         train_samples = 0
         
-        for batch in tqdm(train_loader):
+        for batch in tqdm(train_loader, desc='Train split'):
             images, labels = batch
-            images, labels = images.to(config.device), labels.to(config.device)    
-            optimizer.zero_grad()         
+            images, labels = images.to(config.device), labels.to(config.device)
+            optimizer.zero_grad()
             outputs = model(images)
             _, predictions = torch.max(outputs, 1)
             loss = criterion(outputs, labels)
             loss.backward()
-            optimizer.step()       
+            optimizer.step()
+            
             train_loss += loss.item()
             train_correct += (predictions == labels).sum().item()
             train_samples += labels.size(0)
         
         avg_train_loss = train_loss / len(train_loader)
         avg_train_accuracy = train_correct / train_samples
-
+        
         model.eval()
-           
         val_loss = 0
         val_correct = 0
         val_samples = 0
-
+        
         with torch.no_grad():
-            for batch in tqdm(val_loader):
+            for batch in tqdm(val_loader, desc='Validation split'):
                 images, labels = batch
                 images, labels = images.to(config.device), labels.to(config.device)
                 
@@ -53,16 +56,16 @@ def linear_probe(model, train_loader: DataLoader, val_loader: DataLoader, config
                 
                 val_loss += loss.item()
                 val_correct += (predictions == labels).sum().item()
-                val_samples += labels.size(0)
-        
+                val_samples += labels.size(0) 
+                               
         avg_val_loss = val_loss / len(val_loader)
-        avg_val_accuracy = val_correct / val_samples
+        avg_val_accuracy = val_correct / val_samples  
         
-        print(f"""Epochs: {epoch + 1}/{config.num_epochs} 
-              | train_loss: {avg_train_loss:.4f} 
-              | train_acc: {avg_train_accuracy:.4f} 
-              | val_loss: {avg_val_loss:.4f} 
-              | val_acc: {avg_val_accuracy:.4f}""")
+        print_training_results(epoch, config.num_epochs, avg_train_loss, avg_train_accuracy, avg_val_loss, avg_val_accuracy)
+    
+
+def train_with_distillation(student, teacher, train_loader: DataLoader, val_loader: DataLoader, config):
+    pass
         
 def fine_tune_with_teacher(student, teacher, train_loader: DataLoader, val_loader: DataLoader, config):
     student.to(config.device)
