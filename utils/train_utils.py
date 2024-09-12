4588e4c70d141160d5ee53792c34b6021fa21601
David Poslušný <david.poslusny@email.cz>
2024-09-12 16:01:47 +0200

    feat: train utils

---
+++ utils/train_utils.py
@@ -0,0 +1,9 @@
+def print_training_results(epoch, total_epochs, train_loss, train_accuracy, val_loss, val_accuracy):
+    print(f"""Epochs: {epoch + 1}/{total_epochs} 
+          | train_loss: {train_loss:.4f} 
+          | train_acc: {train_accuracy:.4f} 
+          | val_loss: {val_loss:.4f} 
+          | val_acc: {val_accuracy:.4f}""")
+    
+def print_evaluation_results(test_loss, test_accuracy):
+    print(f"test_loss: {test_loss:.4f} | test_acc: {test_accuracy:.4f}")
