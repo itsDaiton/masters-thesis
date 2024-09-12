f8938c0aa24285ba871cae0c0739721d8f84bbf9
David Poslušný <david.poslusny@email.cz>
2024-09-12 22:41:52 +0200

    refactor: combine train loops

--- src/train.py
+++ src/train.py
@@ -2,43 +2,58 @@ import torch
 from tqdm import tqdm
 from utils.models_utils import get_last_layer
 from utils.train_utils import print_training_results, print_evaluation_results, calculate_hard_distillation
-
     
-def train(model, train_loader, val_loader, config, model_type, mode):
+def train_model(model, train_loader, val_loader, config, architecture, fine_tune=True, with_distillation=False, teacher=None):
     model.to(config.device)
     
-    if mode == 'linear_probing':
+    if with_distillation and teacher is not None:   
+        teacher.to(config.device)
+        teacher.eval()
+        
+    if not fine_tune:
         for param in model.parameters():
             param.requires_grad = False
-        for param in get_last_layer(model, model_type).parameters():
+        for param in get_last_layer(model, architecture).parameters():
             param.requires_grad = True
     else:
         for param in model.parameters():
-            param.requires_grad = True
-    
+            param.requires_grad = True        
+            
     optimizer = config.optimizer(filter(lambda p: p.requires_grad, model.parameters()), lr=config.lr)
     criterion = config.criterion
     
     for epoch in range(config.num_epochs):
         model.train()
+        
         train_loss = 0
         train_correct = 0
         train_samples = 0
         
         for batch in tqdm(train_loader, desc='Train'):
             images, labels = batch
-            images, labels = images.to(config.device), labels.to(config.device)
+            imagees, labels = images.to(config.device), labels.to(config.device)
             optimizer.zero_grad()
+            
+            if with_distillation and teacher is not None:
+                with torch._no_grad():
+                    teacher_outputs = teacher(images)
+                    _, teacher_predictions = torch.max(teacher_outputs, 1)  
+                    
             outputs = model(images)
             _, predictions = torch.max(outputs, 1)
-            loss = criterion(outputs, labels)
+            
+            if with_distillation and teacher is not None:
+                loss = calculate_hard_distillation(outputs, teacher_predictions, labels, criterion)
+            else:
+                loss = criterion(outputs, labels)
+                
             loss.backward()
             optimizer.step()
             
             train_loss += loss.item()
             train_correct += (predictions == labels).sum().item()
             train_samples += labels.size(0)
-        
+            
         avg_train_loss = train_loss / len(train_loader)
         avg_train_accuracy = train_correct / train_samples
         
@@ -52,92 +67,26 @@ def train(model, train_loader, val_loader, config, model_type, mode):
                 images, labels = batch
                 images, labels = images.to(config.device), labels.to(config.device)
                 
+                if with_distillation and teacher is not None:
+                    teacher_outputs = teacher(images)
+                    _, teacher_predictions = torch.max(teacher_outputs, 1)
+                    
                 outputs = model(images)
                 _, predictions = torch.max(outputs, 1)
-                loss = criterion(outputs, labels)
                 
+                if with_distillation and teacher is not None:
+                    loss = calculate_hard_distillation(outputs, teacher_predictions, labels, criterion)
+                else:
+                    loss = criterion(outputs, labels)
+                    
                 val_loss += loss.item()
                 val_correct += (predictions == labels).sum().item()
                 val_samples += labels.size(0) 
-                               
-        avg_val_loss = val_loss / len(val_loader)
-        avg_val_accuracy = val_correct / val_samples  
-        
-        print_training_results(epoch, config.num_epochs, avg_train_loss, avg_train_accuracy, avg_val_loss, avg_val_accuracy)
-    
-def train_with_distillation(student, teacher, train_loader, val_loader, config, model_type, mode):
-    student.to(config.device)
-    teacher.to(config.device)
-    
-    if mode == 'linear_probing':
-        for param in student.parameters():
-            param.requires_grad = False
-        for param in get_last_layer(student, model_type).parameters():
-            param.requires_grad = True
-    else:
-        for param in student.parameters():
-            param.requires_grad = True
+                                      
+            avg_val_loss = val_loss / len(val_loader)
+            avg_val_accuracy = val_correct / val_samples 
             
-    optimizer = config.optimizer(filter(lambda p: p.requires_grad, student.parameters()), lr=config.lr)
-    criterion = config.criterion
-    
-    for epoch in range(config.num_epochs):
-        student.train()
-        teacher.eval()
-        
-        train_loss = 0
-        train_correct = 0
-        train_samples = 0
-        
-        for batch in tqdm(train_loader, desc='Train'): 
-            images, labels = batch
-            images, labels = images.to(config.device), labels.to(config.device)
-            optimizer.zero_grad()
-            
-            with torch.no_grad():
-                teacher_outputs = teacher(images)
-                _, teacher_predictions = torch.max(teacher_outputs, 1)
-                
-            student_outputs = student(images)
-            _, predictions = torch.max(student_outputs, 1)
-             
-            loss = calculate_hard_distillation(student_outputs, teacher_predictions, labels, criterion)    
-            loss.backward()
-            optimizer.step()
-                   
-            train_loss += loss.item()               
-            train_correct += (predictions == labels).sum().item()
-            train_samples += labels.size(0)
-                  
-        avg_train_loss = train_loss / len(train_loader)
-        avg_train_accuracy = train_correct / train_samples
-        
-        student.eval()
-        
-        val_loss = 0
-        val_correct = 0
-        val_samples = 0
-        
-        with torch.no_grad():
-            for batch in tqdm(val_loader, desc='Val'):
-                images, labels = batch
-                images, labels = images.to(config.device), labels.to(config.device)
-                
-                teacher_outputs = teacher(images)
-                _, teacher_predictions = torch.max(teacher_outputs, 1)
-                students_outputs = student(images)
-                _, predictions = torch.max(students_outputs, 1)
-                
-                loss = calculate_hard_distillation(students_outputs, teacher_predictions, labels, criterion)
-           
-                val_loss += loss.item()
-                val_correct += (predictions == labels).sum().item()
-                val_samples += labels.size(0)
-                
-        avg_val_loss = val_loss / len(val_loader)
-        avg_val_accuracy = val_correct / val_samples
-        
-        print_training_results(epoch, config.num_epochs, avg_train_loss, avg_train_accuracy, avg_val_loss, avg_val_accuracy)
+            print_training_results(epoch, config.num_epochs, avg_train_loss, avg_train_accuracy, avg_val_loss, avg_val_accuracy)      
 
 def evaluate(model, test_loader, config):  
     model.to(config.device)
--- utils/models_utils.py
+++ utils/models_utils.py
@@ -5,10 +5,10 @@ model_names = {
     'RegNet': 'facebook/regnet-x-040',
 }
 
-def get_last_layer(model, model_type):
-    if model_type == 'deit':
+def get_last_layer(model, architecture):
+    if architecture == 'deit':
         return model.backbone.cls_classifier
-    elif model_type == 'regnet':
+    elif architecture == 'regnet':
         return model.backbone.classifier[1]
     else:
         return model.backbone.classifier
