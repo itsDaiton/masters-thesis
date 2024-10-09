b5627bcacb4101e8e60648d44ee213547c9d8f0c
David Poslušný <david.poslusny@email.cz>
2024-10-09 21:14:31 +0200

    refactor: customizable config

--- utils/config.py
+++ utils/config.py
@@ -1,12 +1,19 @@
 import torch
 import torch.nn as nn
+from torch.optim import Adam
 
 class Config:
-    """ Configuration class for training hyperparameters. """
-    def __init__(self):
+    def __init__(self, batch_size=8, lr=5e-5, num_epochs=5, optimizer=Adam, binary_task=False, weight_decay=1e-4):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.batch_size = 8
-        self.lr = 5e-5
-        self.num_epochs = 2
-        self.optimizer = torch.optim.AdamW
-        self.criterion = nn.CrossEntropyLoss()
+        self.binary_task = binary_task
+        self.batch_size = batch_size
+        self.lr = lr
+        self.num_epochs = num_epochs
+        self.optimizer = optimizer
+        self.weight_decay = weight_decay
+        
+    def get_loss_function(self):
+        if self.binary_task:
+            return nn.BCEWithLogitsLoss()
+        else:
+            return nn.CrossEntropyLoss()
