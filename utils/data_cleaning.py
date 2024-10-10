492f6881701a4bb37f14d386613db76dc2a9a538
David Poslušný <david.poslusny@email.cz>
2024-10-10 18:07:46 +0200

    fix: fix sun397 label cleaning

--- utils/data_cleaning.py
+++ utils/data_cleaning.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset, ClassLabel
 from data.country_codes import get_country_codes
+from data.mappings.label_mappings import pcam, sun397
 
 def clean_labels(dataset, name):
     """ Custom label cleaning function specific for each dataset.""" 
@@ -11,11 +12,16 @@ def clean_labels(dataset, name):
         clean_labels = [s.lower() for s in clean_labels]
         
         if name == 'pcam':
-            clean_labels = ['healthy lymph node tissue', 'lymph node tumor tissue'] 
+            mappings = pcam
+            clean_labels = [mappings.get(label, label) for label in labels]
             
         if name == 'country211':
             mappings = get_country_codes()
-            clean_labels = [mappings.get(label, label) for label in labels]   
+            clean_labels = [mappings.get(label, label) for label in labels]
+            
+        if name == 'sun397':
+            mappings = sun397
+            clean_labels = [mappings.get(label, label) for label in labels]
         
         label_mapping = {i: clean_labels[i] for i in range(len(labels))}
         features_copy = dataset[split].features.copy()
