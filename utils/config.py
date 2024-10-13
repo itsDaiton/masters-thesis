a2eb15c757d98c5a5b1717b56dd19aa895377cd3
David Poslušný <david.poslusny@email.cz>
2024-10-13 17:34:32 +0200

    fix: loss function fix

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,7 @@ from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 
 class Config:
-    def __init__(self, batch_size=8, lr=5e-5, num_epochs=5, optimizer=Adam, criterion=CrossEntropyLoss, weight_decay=1e-4):
+    def __init__(self, batch_size=8, lr=5e-5, num_epochs=5, optimizer=Adam, criterion=CrossEntropyLoss(), weight_decay=1e-4):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.criterion = criterion
         self.batch_size = batch_size
