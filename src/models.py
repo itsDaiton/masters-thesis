e4373832a238f549a0cddf3f5e6196bbdc12d0bc
David Poslušný <david.poslusny@email.cz>
2024-11-05 12:27:18 +0100

    feat: add option for linear probe clip

--- src/models.py
+++ src/models.py
@@ -20,9 +20,10 @@ class Backbone(nn.Module):
 class CLIP(nn.Module):
     """ CLIP model pre-trained on YFCC100M dataset. """
     
-    def __init__(self, model_name=model_names['CLIP'], for_training=False, num_classes=None):
+    def __init__(self, model_name=model_names['CLIP'], for_training=False, num_classes=None, linear_probe=False):
         super(CLIP, self).__init__()
         self.for_training = for_training
+        self.linear_probe = linear_probe
         
         if for_training and num_classes is not None:
             self.model = AutoModelForImageClassification.from_pretrained(
@@ -34,9 +35,22 @@ class CLIP(nn.Module):
             self.model = CLIPModel.from_pretrained(pretrained_model_name_or_path=model_name)
  
     def forward(self, images, texts=None):   
-        if self.for_training and not self.use_coop:
+        if self.for_training:
             outputs = self.model(pixel_values=images)
             return outputs.logits
+        elif self.linear_probe:
+            outputs = self.model(pixel_values=images, input_ids=texts)
+            return outputs.image_embeds, outputs.text_embeds
         else:
             outputs = self.model(pixel_values=images, input_ids=texts)
             return outputs.logits_per_image
+        
+class LinearClassifier(nn.Module):
+    """ Linear classifier trained on top of CLIP's image and text embeddings. """
+    
+    def __init__(self, in_features, num_classes):
+        super(LinearClassifier, self).__init__()
+        self.fc = nn.Linear(in_features, num_classes)
+        
+    def forward(self, x):
+        return self.fc(x)
