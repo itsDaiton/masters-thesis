bfee75c714b666be572bdaceebf15a2e22944599
itsDaiton <david.poslusny@email.cz>
2024-12-30 15:58:19 +0100

    feat: add optional dropout

--- src/models.py
+++ src/models.py
@@ -6,13 +6,19 @@ from utils.models_utils import model_names
 class Backbone(nn.Module):
     """Base class for all backbone models used - ViT, DeiT, Swin, RegNet (for distillation)."""
 
-    def __init__(self, model_name, num_classes):
+    def __init__(self, model_name, num_classes, dropout=False, dropout_rate=0.1):
         super(Backbone, self).__init__()
         self.backbone = AutoModelForImageClassification.from_pretrained(
             pretrained_model_name_or_path=model_name,
             num_labels=num_classes,
             ignore_mismatched_sizes=True,
         )
+        if dropout:
+            in_features = self.backbone.classifier.in_features
+            self.backbone.classifier = nn.Sequential(
+                nn.Dropout(p=dropout_rate),
+                nn.Linear(in_features, num_classes),
+            )
 
     def forward(self, x):
         outputs = self.backbone(x)
@@ -23,7 +29,12 @@ class CLIP(nn.Module):
     """CLIP model pre-trained on YFCC100M dataset."""
 
     def __init__(
-        self, model_name=model_names["CLIP"], for_training=False, num_classes=None
+        self,
+        model_name=model_names["CLIP"],
+        for_training=False,
+        num_classes=None,
+        dropout=False,
+        dropout_rate=0.1,
     ):
         super(CLIP, self).__init__()
         self.for_training = for_training
@@ -37,6 +48,12 @@ class CLIP(nn.Module):
         else:
             self.model = CLIPModel.from_pretrained(
                 pretrained_model_name_or_path=model_name
+            )
+        if dropout:
+            in_features = self.model.classifier.in_features
+            self.model.classifier = nn.Sequential(
+                nn.Dropout(p=dropout_rate),
+                nn.Linear(in_features, num_classes),
             )
 
     def forward(self, images, texts=None):
